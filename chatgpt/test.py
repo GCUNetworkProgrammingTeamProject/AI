9e642ee5fe1ac2a2276addeee3d6b203bfd0705b
d3ng03 <d3ng03@gmail.com>
2023-10-26 02:10:44 +0900

    remove api-key

--- chatgpt/test.py
+++ chatgpt/test.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = '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'
+openai.api_key = ''
 
 messages = []
 content = "코끼리가 뭐야?"
@@ -13,7 +13,7 @@ completion = openai.ChatCompletion.create(
 )
 com = completion
 res = completion.choices[0].message.content
-print(com.usage)
+print(com)
 
 messages.append({"role": "assistant", "content": res})
 print(messages)
